3fa894abb499feb9ba4c0388dd851e6d80086a90
cadu_linux <carlospegorarolopes@gmail.com>
2024-01-17 23:46:57 -0300

    next to nuxt

---
+++ site/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bg-stone-900">
+    <div class="absolute bg-stone-900 w-full z-0">
+      <img class="w-full z-0" src="~/assets/heroSectionBg.png" alt="banner">
+    </div>
+    <h1 class="z-50 text-white">a</h1>
+  </div>
+</template>
